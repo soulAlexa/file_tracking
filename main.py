675e343dba522efffa501a0b8820bed968670026
soulAlexa <volertt48@gmail.com>
2024-04-22 21:30:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print(213)
+filename = "test_ser2.docx"
+file = open(filename, "rb")
+file2 = open("test.txt", "w")
+while True:
+    file_data = file.read(36)
+    file2.write(str(file_data))
+    file2.write("\n")
+    if not file_data:
+        break
