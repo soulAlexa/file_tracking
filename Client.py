0c52d56c6fe9817354e6c7d1e3139b44771de039
soulAlexa <volertt48@gmail.com>
2024-04-22 21:30:53 +0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,17 +1,36 @@
 import socket
-
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLineEdit, QMessageBox, QPushButton, QVBoxLayout, QWidget
+import sys
+from _thread import *
 
 class prot_client():
-    def __init__(self):
+    def __init__(self, window):
         HOST = '127.0.0.1'
         PORT = 2000
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((HOST, PORT))
-        print(111111111)
+        # self.authorization_con()
         self.management()
         print("3234234")
 
-    def management(self):
+    def authorization_con(self):#добавить шифрование
+        data_send = str((window._login.text(), window._passw.text()))
+        print(data_send)
+        data_send = data_send.encode()
+        self.sock.sendall(data_send)
+        data_recv = self.sock.recv(2)
+        data_recv = int.from_bytes(data_recv, 'little', signed=True)
+        if data_recv:
+            return True
+        else:
+            return False
+
+    def management(self):#добавить шифрование
+        if self.authorization_con():
+            pass
+        else:
+            exit_thread()
         while True:
             data_recv = self.sock.recv(1024)
             data_recv = data_recv.decode()
@@ -19,9 +38,10 @@ class prot_client():
             data_send = "Диалог"
             data_send = data_send.encode()
             self.sock.sendall(data_send)
-            self.fille_accept()
+            self.accept()
+            print(123123)
 
-    def accept(self):
+    def accept(self):#добавить шифрование
         while True:
             data = input("Напишите сообщение: ")
             data_bytes = data.encode()
@@ -30,7 +50,7 @@ class prot_client():
             data = data_bytes.decode()
             print("Получено сообщение: ", data)
 
-    def fille_accept(self):
+    def fille_accept(self):#добавить шифрование
         filename = "test_client2.docx"
         file = open(filename, "wb")
         size_fill = 0
@@ -51,7 +71,7 @@ class prot_client():
                 file_data = self.sock.recv(4096)
             file.write(file_data)
 
-    def fille_send(self):
+    def fille_send(self):#добавить шифрование
         filename = "server.docx"
         file = open(filename, "rb")
         byte_len = (len(file.read())).to_bytes(8, 'big')
@@ -65,5 +85,59 @@ class prot_client():
         print("file sended")
 
 
+class Login_Window(QMainWindow):
+    _login : QLineEdit
+    _passw : QLineEdit
+    _b1 : QPushButton
+    # _db_name : QLineEdit
+
+    def __init__(self):
+        super().__init__()
+
+        # logger.add("file_X.log", retention="10 days", format="{time:MMMM D, YYYY > HH:mm:ss} | {level} | {message}")
+
+        self.setGeometry(800, 300, 250, 200)
+        self.setWindowIcon(QIcon('icon.png'))
+        self._set_face()
+
+    def _set_face(self):
+        title = "Вход"
+        self.setWindowTitle(title)
+
+        v1 = QVBoxLayout()
+        self._login = QLineEdit('root')
+        self._passw = QLineEdit('11111111')
+        self._passw.setEchoMode(QLineEdit.Password)
+        # self._db_name = QLineEdit('mydata')
+        self._b1 = QPushButton("Connect")
+        self._b1.clicked.connect(self.activ_prot)
+
+        v1.addWidget(self._login)
+        v1.addWidget(self._passw)
+        # v1.addWidget(self._db_name)
+        v1.addWidget(self._b1)
+
+        w = QWidget()
+        w.setLayout(v1)
+        self.setCentralWidget(w)
+
+    def activ_prot(self):
+        start_new_thread(prot_client, (self, ))
+        print(222222222222222222222)
+        # window.hide()
+        # a = prot_client(self)
+
+
+
 if __name__ == '__main__':
-    a = prot_client()
+    # a = prot_client()
+
+    app = QApplication(sys.argv)
+
+    with open('style1.qss', 'r') as f:
+        style = f.read()
+
+    app.setStyleSheet(style)
+    window = Login_Window()
+    window.show()
+    app.exec()
